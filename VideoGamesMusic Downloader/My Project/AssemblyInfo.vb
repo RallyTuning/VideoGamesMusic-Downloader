0038223d786f798ac1fa7f452f9ea7966cf61dff
Gianluigi <50233545+RallyTuning@users.noreply.github.com>
2022-10-26 16:43:04 +0200

    Aggiunte info licenza

--- VideoGamesMusic Downloader/My Project/AssemblyInfo.vb
+++ VideoGamesMusic Downloader/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' usando l'asterisco '*' come illustrato di seguito:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.2022.10.07")>
-<Assembly: AssemblyFileVersion("1.2022.10.07")>
+<Assembly: AssemblyVersion("2.2022.10.26")>
+<Assembly: AssemblyFileVersion("2.2022.10.26")>
--- VideoGamesMusic Downloader/Principale.vb
+++ VideoGamesMusic Downloader/Principale.vb
@@ -96,10 +96,19 @@ Public Class Principale
     End Sub
 
     Private Sub Lnk_Info_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles Lnk_Info.LinkClicked
-        If MessageBox.Show(String.Format("{1}{0}{2}{0}{0}{3}{0}{4}{0}{0}{5}", Environment.NewLine, "Made with ♥ by Gianluigi Capozzoli",
+        If MessageBox.Show(String.Format("{1}{0}{2}{0}{0}{3}{0}{4}{0}{0}{5}{0}{0}{6}", Environment.NewLine, "Made with ♥ by Gianluigi Capozzoli",
                                          "www.disactive.com - www.capozzoli.me",
                                          "Suondtrack by downloads.khinsider.com",
                                          "Thanks them for the songs and me for the automation :P",
+                                         "MIT License
+
+Copyright (c) 2022 Gianluigi Capozzoli
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.",
                                          "Would you visit the GitHub page?"),
                            "About that shit...", MessageBoxButtons.YesNo, MessageBoxIcon.Information) = DialogResult.Yes Then
             Process.Start("https://github.com/RallyTuning/VideoGamesMusic-Downloader")
